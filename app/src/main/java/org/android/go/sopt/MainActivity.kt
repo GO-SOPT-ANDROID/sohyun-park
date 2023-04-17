269a42a25aea1ac8e799955fc1d0413ba7960143
Sohyun <so405@naver.com>
2023-04-17 19:02:11 +0900

    Feat: 바텀 네비게이션 기능

--- app/src/main/java/org/android/go/sopt/MainActivity.kt
+++ app/src/main/java/org/android/go/sopt/MainActivity.kt
@@ -2,8 +2,11 @@ package org.android.go.sopt
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import org.android.go.sopt.databinding.ActivityMainBinding
+import org.android.go.sopt.home.GalleryFragment
 import org.android.go.sopt.home.HomeFragment
+import org.android.go.sopt.home.SearchFragment
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -12,11 +15,40 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val currentFragment = supportFragmentManager.findFragmentById(R.id.fcv_main)
-        if (currentFragment == null) {
-            supportFragmentManager.beginTransaction().add(R.id.fcv_main, HomeFragment()).commit()
-        }
-
-
+        startFragment(HomeFragment())
+        clickNavigationItem()
     }
+
+
+    private fun clickNavigationItem() {
+        binding.bnvMain.setOnItemSelectedListener { item ->
+            changeFragment(
+                when (item.itemId) {
+                    R.id.menu_home -> {
+                        HomeFragment()
+                    }
+                    R.id.menu_gallery -> {
+                        GalleryFragment()
+                    }
+                    else -> {
+                        SearchFragment()
+                    }
+                }
+            )
+            true
+        }
+    }
+
+    private fun changeFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fcv_main, fragment)
+            .commit()
+    }
+
+    private fun startFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .add(R.id.fcv_main, fragment)
+            .commit()
+    }
+
 }
