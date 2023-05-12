36b33ad824c37a97ac38a0a57f2b767fcf214211
Sohyun <so405@naver.com>
2023-05-12 16:37:55 +0900

    Feat: 서버통신 로직 구현

--- app/src/main/java/org/android/go/sopt/SignUpActivity.kt
+++ app/src/main/java/org/android/go/sopt/SignUpActivity.kt
@@ -1,51 +1,27 @@
 package org.android.go.sopt
 
 import android.content.Context
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.inputmethod.InputMethodManager
 import org.android.go.sopt.databinding.ActivitySignUpBinding
+import retrofit2.Call
+import retrofit2.Response
 
 
 class SignUpActivity : AppCompatActivity() {
     private lateinit var binding: ActivitySignUpBinding
+    private val signUpService = ServicePool.signUpService
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySignUpBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        signUpComplete()
+        completeSignUp()
 
         binding.root.setOnClickListener {
             hideKeyboard()
-        }
-
-    }
-
-
-    private fun signUpComplete() {
-
-        val intent = Intent(this, SignInActivity::class.java)
-
-        binding.btnSignupComplete.setOnClickListener {
-
-            if (binding.etSignupId.length() in 6..10 && binding.etSignupPw.length() in 8..12
-                && binding.etSignupName.text.isNotEmpty() && binding.etSignupSpecialty.text.isNotEmpty()
-            ) {
-
-                binding.root.showSnackbar("회원가입에 성공했습니다.")
-
-                intent.putExtra("id", binding.etSignupId.text.toString())
-                intent.putExtra("pw", binding.etSignupPw.text.toString())
-                intent.putExtra("name", binding.etSignupName.text.toString())
-                intent.putExtra("specialty", binding.etSignupSpecialty.text.toString())
-                setResult(RESULT_OK, intent)
-                finish()
-            } else {
-                binding.root.showSnackbar("회원가입에 실패했습니다.")
-            }
-
         }
 
     }
@@ -63,5 +39,48 @@ class SignUpActivity : AppCompatActivity() {
         }
     }
 
+    private fun canUserSignUp(): Boolean {
+        return binding.etSignupId.text.length in 6..10
+                && binding.etSignupPw.text.length in 8..12
+                && binding.etSignupName.text.isNotBlank()
+                && binding.etSignupSpecialty.text.isNotBlank()
+    }
+
+
+    private fun completeSignUp() {
+        signUpService.login(
+            with(binding) {
+                RequestSignUpDto(
+                    etSignupId.text.toString(),
+                    etSignupPw.text.toString(),
+                    etSignupName.text.toString(),
+                    etSignupSpecialty.text.toString()
+                )
+            }
+        ).enqueue(object : retrofit2.Callback<ResponseSignUpDto> {
+            override fun onResponse(
+                call: Call<ResponseSignUpDto>,
+                response: Response<ResponseSignUpDto>,
+            ) {
+                if (response.isSuccessful) {
+                    response.body()?.message?.let { binding.root.showToast(it) }
+                        ?: "회원가입에 성공했습니다."
+                    finish()
+
+                    if (!isFinishing) finish()
+                } else {
+                    // 응답 실패!
+                    response.body()?.message?.let { binding.root.showToast(it) }
+                        ?: "서버통신 실패(40X)"
+                }
+            }
+
+            override fun onFailure(call: Call<ResponseSignUpDto>, t: Throwable) {
+                // 왜 안 오지?!
+                t.message?.let { binding.root.showToast(it) } ?: "서버통신 실패(응답값 X)"
+            }
+        })
+
+    }
 
 }
