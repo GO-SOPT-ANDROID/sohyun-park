7f4649d95e420c66cb98469dc702949a3fbc1685
Sohyun <so405@naver.com>
2023-06-01 17:04:08 +0900

    [FEAT] 확장함수 추가

--- app/src/main/java/org/android/go/sopt/util/ContextUtil.kt
+++ app/src/main/java/org/android/go/sopt/util/ContextUtil.kt
@@ -1,8 +1,14 @@
-package org.android.go.sopt
+package org.android.go.sopt.util
 
+import android.content.Context
+import android.util.Log
 import android.view.View
 import android.widget.Toast
+import androidx.recyclerview.widget.DiffUtil
 import com.google.android.material.snackbar.Snackbar
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 
 fun View.showSnackbar(message: String) {
@@ -13,4 +19,39 @@ fun View.showSnackbar(message: String) {
 
 fun View.showToast(message: String) {
     Toast.makeText(this.context, message, Toast.LENGTH_SHORT).show()
+}
+
+
+fun <ResponseType> Call<ResponseType>.enqueueUtil(
+    onSuccess: (ResponseType) -> Unit,
+    onError: ((stateCode: Int) -> Unit)? = null,
+) {
+    this.enqueue(object : Callback<ResponseType> {
+        override fun onResponse(call: Call<ResponseType>, response: Response<ResponseType>) {
+            if (response.isSuccessful) {
+                onSuccess.invoke(response.body() ?: return)
+            } else {
+                Log.d("Hello", "error:${response.message()}")
+                onError?.invoke(response.code())
+            }
+        }
+
+        override fun onFailure(call: Call<ResponseType>, t: Throwable) {
+            Log.d("NetworkTest", "error:$t")
+        }
+    })
+}
+
+
+class ItemDiffCallback<T : Any>(
+    val onItemsTheSame: (T, T) -> Boolean,
+    val onContentsTheSame: (T, T) -> Boolean
+) : DiffUtil.ItemCallback<T>() {
+    override fun areItemsTheSame(oldItem: T, newItem: T): Boolean = onItemsTheSame(oldItem, newItem)
+    override fun areContentsTheSame(oldItem: T, newItem: T): Boolean =
+        onContentsTheSame(oldItem, newItem)
+}
+
+fun Context.toast(message: String) {
+    Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
 }
